6788cf5326315393240300a7a939d91e95049b0e
ayangzy <ayangefelix8@gmail.com>
2020-02-27 10:39:57 -0800

    set up, listings and realtors pages added

---
+++ listings/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Listing
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor')
+    list_display_links = ('id', 'title')
+    list_filter = ('realtor', )
+    list_editable = ('is_published',)
+    search_fields = ('title', 'price', 'description', 'address', 'city', 'zipcode', 'state')
+    list_per_page = 25
+# Register your models here.
+
+admin.site.register(Listing, ListingAdmin)
