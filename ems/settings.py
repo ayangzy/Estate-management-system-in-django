7754af80d80f3c9c4e0b21ff9fbf9927ed9d0cdd
ayangzy <ayangefelix8@gmail.com>
2020-03-01 14:03:38 -0800

    accountsApp, urls, register and login templates added

--- ems/settings.py
+++ ems/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'pages.apps.PagesConfig',
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
+
 ]
 
 MIDDLEWARE = [
