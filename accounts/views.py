b10e55487bbea1b0e7c8dbb8ecc52ad2eb0c82eb
ayangzy <ayangefelix8@gmail.com>
2020-03-02 01:34:39 -0800

    user login added

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib import auth
 
 
 # Create your views here.
@@ -42,7 +43,20 @@ def register(request):
 
 
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('dashboard')
+        else:
+            return render(request, 'accounts/login.html', {'error': 'username or password is incorrect'})
+
+    else:
+        return render(request, 'accounts/login.html')
 
 
 def logout(request):
